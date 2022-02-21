9bb25c8938c202d1770172ee9d654cc6346d7434
DanielaCordova <dancordo@ucm.es>
2022-02-21 23:23:08 +0100

    Inicio de libro.html

--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -38,4 +38,9 @@ public class RootController {
     public String mensajeria(Model model) {
         return "mensajeria";
     }
+
+    @GetMapping("/libro")
+    public String libro(Model model) {
+        return "libro";
+    }
 }
