b62b98599bdc1b2e3b402cec84bd24497c23ddca
DanielaCordova <dancordo@ucm.es>
2022-05-25 19:09:09 +0200

    Update AdminController.java

--- src/main/java/es/ucm/fdi/iw/controller/AdminController.java
+++ src/main/java/es/ucm/fdi/iw/controller/AdminController.java
@@ -93,7 +93,7 @@ public class AdminController {
         .stream().findFirst().orElse(null);
         */
 
-        List<Review> reviewBook = entityManager.createNamedQuery("Review.allBook", Review.class)
+        List<Review> reviewBook = entityManager.createNamedQuery("Review.byBook", Review.class)
         .setParameter("id", id)
         .getResultList();
         
